06f0174e66ea42c909231299befbf15b1a32bfaf
XDouglasX7 <83845482+XDouglasX7@users.noreply.github.com>
2021-11-25 19:00:41 -0300

    Add files via upload

---
+++ Controllers/ColaboradoresController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LibrarySystem.Models;
+using LibrarySystem.Models.Dominio;
+
+namespace LibrarySystem.Controllers
+{
+    public class ColaboradoresController : Controller
+    {
+        private readonly Contexto _context;
+
+        public ColaboradoresController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: Colaboradores
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Colaboradores.ToListAsync());
+        }
+
+        // GET: Colaboradores/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var colaborador = await _context.Colaboradores
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (colaborador == null)
+            {
+                return NotFound();
+            }
+
+            return View(colaborador);
+        }
+
+        // GET: Colaboradores/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Colaboradores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,nome,cpf,email,usuario")] Colaborador colaborador)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(colaborador);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(colaborador);
+        }
+
+        // GET: Colaboradores/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var colaborador = await _context.Colaboradores.FindAsync(id);
+            if (colaborador == null)
+            {
+                return NotFound();
+            }
+            return View(colaborador);
+        }
+
+        // POST: Colaboradores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,nome,cpf,email,usuario")] Colaborador colaborador)
+        {
+            if (id != colaborador.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(colaborador);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ColaboradorExists(colaborador.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(colaborador);
+        }
+
+        // GET: Colaboradores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var colaborador = await _context.Colaboradores
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (colaborador == null)
+            {
+                return NotFound();
+            }
+
+            return View(colaborador);
+        }
+
+        // POST: Colaboradores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var colaborador = await _context.Colaboradores.FindAsync(id);
+            _context.Colaboradores.Remove(colaborador);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ColaboradorExists(int id)
+        {
+            return _context.Colaboradores.Any(e => e.id == id);
+        }
+    }
+}
